88364bef58594ac02046a333303a4350a3134e04
dgaust <dgaust@outlook.com>
2019-09-23 12:59:47 +1000

    Update groundstatus.py

--- groundstatus.py
+++ groundstatus.py
@@ -19,23 +19,27 @@ def createjson():
     html = f.read()
     parsed_html = BeautifulSoup(html, "lxml")
     groundlist = []
-    li = parsed_html.find('table', {'summary': 'SportsGrounds '})
-    for table_row in li:
-        cells = table_row.findAll('td')
-        try:
-           if len(cells) > 0:
-              park_name = cells[0].text.strip()
-              park_status = cells[4].text.strip()
-              park_comment = cells[3].text.strip()
-              park = {'park_name': park_name.encode('utf-8'), 'park_status': park_status.encode('utf-8'), 'park_comment': park_comment.encode('utf-8'), 'updated' : time_run}
-              groundlist.append(park)
-           else:
-              print "no cells"
-          # ftpupload()
-        except:
-           print('error')
-    with open('test.json', 'wb') as outfile:
-         json.dump(groundlist, outfile)
+    
+    tags = parsed_html.findAll("div", class_="sportsgrounds__item")
+    print(tags)
+    
+   # li = parsed_html.find('table', {'summary': 'SportsGrounds '})
+   # for table_row in li:
+   #     cells = table_row.findAll('td')
+   #     try:
+   #        if len(cells) > 0:
+   #           park_name = cells[0].text.strip()
+   #           park_status = cells[4].text.strip()
+   #           park_comment = cells[3].text.strip()
+   #           park = {'park_name': park_name.encode('utf-8'), 'park_status': park_status.encode('utf-8'), 'park_comment': park_comment.encode('utf-8'), 'updated' : time_run}
+   #          groundlist.append(park)
+   #        else:
+   #           print "no cells"
+   #       # ftpupload()
+   #     except:
+   #        print('error')
+   # with open('test.json', 'wb') as outfile:
+   #      json.dump(groundlist, outfile)
  
     ftpupload()
 def ftpupload():
