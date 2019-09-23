16039ecea4988b1db2b43e39bbbfdf0a098bf085
dgaust <dgaust@outlook.com>
2019-09-23 13:06:30 +1000

    Update groundstatus.py

--- groundstatus.py
+++ groundstatus.py
@@ -22,10 +22,10 @@ def createjson():
     tags = parsed_html.findAll("div", class_="sportsgrounds__item")
     #print(tags)
     for t in tags:
-        #park_name = t[0].text
+        park_name = t.findAll("div", class_="sportsgrounds__name")
         #park_status = t[1].text
-        print(t)
-        #print(park_name)
+        #print(t)
+        print(park_name)
     
    # li = parsed_html.find('table', {'summary': 'SportsGrounds '})
    # for table_row in li:
