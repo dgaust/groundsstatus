ceb983b6ab3294ad8ab373eac5b8d454d37d488d
dgaust <dgaust@outlook.com>
2019-09-23 14:00:14 +1000

    Add files via upload

---
+++ sensor.php
@@ -0,0 +1,20 @@
+<?php
+$park = null;
+if (isset($_GET['park'])) 
+{
+    $park = $_GET['park'];
+    $data = file_get_contents('https://russellvalefootball.com/test.json');
+    $characters = json_decode($data); 
+    foreach ($characters as $item) 
+    {
+       if ($item->park_name == $park)
+       { 
+        echo json_encode($item);
+       }
+    }
+}
+else
+{
+    echo "You need to specify a park in the URL using the format ?park={park name}.";
+}
+?>
