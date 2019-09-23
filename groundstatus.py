35f53f37ddfbe81ef9113ce87aa21a44248a9193
dgaust <dgaust@outlook.com>
2019-09-23 13:55:29 +1000

    Add files via upload

--- groundstatus.py
+++ groundstatus.py
@@ -13,40 +13,28 @@ def createjson():
     time_run = datetime.datetime.now()
     #time_run = time_run.isoformat()
     time_run = time_run.strftime('%d %b %Y, %-I:%M %p')
-    url = "https://wollongong.nsw.gov.au/explore/sport-and-recreation/sportsgrounds"
-    print(url)
+    url = "http://wollongong.nsw.gov.au/facilities/sportrec/Pages/sportsgrounds.aspx"
     f = urllib.urlopen(url)
     html = f.read()
     parsed_html = BeautifulSoup(html, "lxml")
     groundlist = []
-    tags = parsed_html.findAll("div", class_="sportsgrounds__item")
-    #print(tags)
-    for t in tags:
-        park_name = t.findAll("div", class_="sportsgrounds__name")[0].text.split(",")[0]
-        park_status = t.findAll("div", class_="sportsgrounds__status")[0].text.strip('Status:  ')
-        park_comment = t.findAll("div", class_="sportsgrounds__more-info")[0].text      
-        park = {'park_name': park_name.encode('utf-8'), 'park_status': park_status.encode('utf-8'), 'park_comment': park_comment.encode('utf-8'), 'updated' : time_run}
-        print(park)
-        groundlist.append(park)
+    li = parsed_html.find('table', {'summary': 'SportsGrounds '})
+    for table_row in li:
+        cells = table_row.findAll('td')
+        try:
+           if len(cells) > 0:
+              park_name = cells[0].text.strip()
+              park_status = cells[4].text.strip()
+              park_comment = cells[3].text.strip()
+              park = {'park_name': park_name.encode('utf-8'), 'park_status': park_status.encode('utf-8'), 'park_comment': park_comment.encode('utf-8'), 'updated' : time_run}
+              groundlist.append(park)
+           else:
+              print "no cells"
+          # ftpupload()
+        except:
+           print('error')
     with open('test.json', 'wb') as outfile:
          json.dump(groundlist, outfile)
-   # li = parsed_html.find('table', {'summary': 'SportsGrounds '})
-   # for table_row in li:
-   #     cells = table_row.findAll('td')
-   #     try:
-   #        if len(cells) > 0:
-   #           park_name = cells[0].text.strip()
-   #           park_status = cells[4].text.strip()
-   #           park_comment = cells[3].text.strip()
-   #           park = {'park_name': park_name.encode('utf-8'), 'park_status': park_status.encode('utf-8'), 'park_comment': park_comment.encode('utf-8'), 'updated' : time_run}
-   #          groundlist.append(park)
-   #        else:
-   #           print "no cells"
-   #       # ftpupload()
-   #     except:
-   #        print('error')
-   # with open('test.json', 'wb') as outfile:
-   #      json.dump(groundlist, outfile)
  
     ftpupload()
 def ftpupload():
