5938488f896389e2ba3d989661302012593f83d3
dgaust <dgaust@outlook.com>
2020-02-12 08:30:38 +1100

    Update groundstatus_nobs4.py

--- groundstatus_nobs4.py
+++ groundstatus_nobs4.py
@@ -1,8 +1,11 @@
+#! /usr/bin/env python
+
 from lxml import html
 import requests
 
 page = requests.get('https://www.wollongong.nsw.gov.au/explore/sport-and-recreation/sportsgrounds')
 tree = html.fromstring(page.content)
+print 'Tree:', tree
 grounds = buyers = tree.xpath('//div[@title="portsgrounds__item"]/text()')
 
 print 'Grounds: ', grounds
